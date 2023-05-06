87eaa86364174a392c02e14b3084c41a04d715e2
David Huggett <huggett_david@hotmail.com>
2023-05-06 11:19:47 +0100

    Version update

--- UI/Web/My Project/AssemblyInfo.vb
+++ UI/Web/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.13.1.0")>
-<Assembly: AssemblyFileVersion("1.13.1.0")>
+<Assembly: AssemblyVersion("1.13.2.0")>
+<Assembly: AssemblyFileVersion("1.13.2.0")>
